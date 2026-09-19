eaaa23865ac1fc858ec4e4cf695c2a3776038161
TuxeBro <78770366+TBroz15@users.noreply.github.com>
2026-09-19 19:27:59 +0800

    mark mod as only for server side stuff

--- src/main/java/dev/tuxebro/async_opac_compat/AOCMain.java
+++ src/main/java/dev/tuxebro/async_opac_compat/AOCMain.java
@@ -1,12 +1,13 @@
 package dev.tuxebro.async_opac_compat;
 
 import com.mojang.logging.LogUtils;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import org.slf4j.Logger;
 
-@Mod(AOCMain.MOD_ID)
+@Mod(value = AOCMain.MOD_ID, dist = Dist.DEDICATED_SERVER)
 public class AOCMain {
     public static final String MOD_ID = "async_opac_compat";
     private static final Logger LOGGER = LogUtils.getLogger();
